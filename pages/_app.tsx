fbde025859803ea0f2f9c64af30c3b2b4b46033b
yadavamit2611 <thisisamit.kumar21@gmail.com>
2023-09-08 01:05:53 +0200

    search news - news api - api route backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import { Container } from 'react-bootstrap'
+import styles from "@/styles/App.module.css"
+const inter = Inter({subsets:['latin']})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<div className={inter.className}>
+      <Head>
+        <title key="title">News App</title>
+        <meta name="description" key="description" content="NextJS crash course for news-app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container className={styles.pageContainer}>
+        <Component {...pageProps} />
+      </Container>
+    </div>)
 }
---
+++ pages/search.tsx
@@ -0,0 +1,53 @@
+import NewsArticlesGrid from "@/components/NewsArticlesGrid";
+import { NewsArticle, NewsResponse } from "@/models/NewsArticles";
+import {FormEvent, useState} from "react"
+import {Form, Button,Spinner } from "react-bootstrap"
+
+const SearchNewsPage = () => {
+    const [searchResults, setSearchResults] = useState<NewsArticle[] | null>(null);
+    const [searchResultsLoading, setSearchResultsLoading] = useState(false);
+    const [searchResultsLoadingisError, setSearchResultsLoadingisError] = useState(false);
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const searchQuery = formData.get("searchQuery")?.toString().trim();
+        if(searchQuery){
+            try {
+                setSearchResults(null);
+                setSearchResultsLoadingisError(false);
+                setSearchResultsLoading(true);
+                const response = await fetch("/api/search-news?q="+searchQuery);
+                console.log(response);
+                const articles: NewsArticle[] = await response.json();
+                console.log(articles);
+                setSearchResults(articles);
+            } catch (error) {
+                console.error(error);
+                setSearchResultsLoadingisError(true);
+            }finally{
+                setSearchResultsLoading(false);
+            }
+        }
+    }
+
+    return (<main>
+        <h1>Search News</h1>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="search-input">
+                <Form.Label>Search Query</Form.Label>
+                <Form.Control name="searchQuery" placeholder="E.g politics, sports"></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Button type="submit" className="mb-3" disabled={searchResultsLoading}>Search</Button>
+            </Form.Group>
+        </Form>
+        <div className="d-flex flex-column align-items-center">
+            {searchResultsLoading && <Spinner animation="border"/>}
+            {searchResultsLoadingisError && <p>Something went wrong. Please try again later..</p>}
+            {searchResults?.length === 0 && <p>Nothing found. Try a different query</p>}
+            {searchResults && <NewsArticlesGrid articles={searchResults}/>}
+        </div>
+    </main>)
+}
+export default SearchNewsPage;
